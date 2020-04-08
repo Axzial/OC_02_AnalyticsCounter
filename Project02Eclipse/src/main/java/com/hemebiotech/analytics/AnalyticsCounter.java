71d6eed85a754af82a32fadec43bd2bd46c7474e
Victor Brown <victorbrown37@protonmail.com>
2020-04-08 12:43:11 +0200

    Added a launcher class (AnalyticsCounter) and send args to it, also created base components in Main class

---
+++ Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -0,0 +1,19 @@
+package com.hemebiotech.analytics;
+
+public class AnalyticsCounter {
+
+    /**
+     *
+     * @param args are the args received from main
+     */
+    public AnalyticsCounter(String[] args){
+
+    }
+
+    /**
+     * App launcher, executing all needed tasks
+     */
+    public void launcher(){
+
+    }
+}
--- Project02Eclipse/src/main/java/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/main/java/com/hemebiotech/analytics/Main.java
@@ -1,8 +1,27 @@
 package com.hemebiotech.analytics;
 
 public class Main {
-	
-	public static void main(String args[]) throws Exception {
 
+	private static Main instance;
+
+	public static Main getInstance(){
+		return instance;
+	}
+
+	/**
+	 * Main entry point
+	 * @param args received
+	 * @throws Exception
+	 */
+	public static void main(String args[]){
+		init();
+		new AnalyticsCounter(args).launcher();
+	}
+
+	/**
+	 * Init base components
+	 */
+	public static void init(){
+		instance = new Main();
 	}
 }
