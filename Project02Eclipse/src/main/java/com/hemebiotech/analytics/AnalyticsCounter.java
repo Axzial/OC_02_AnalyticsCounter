78302fd86d43df129c50f60f8897f2e494967492
Victor Brown <victorbrown37@protonmail.com>
2020-04-15 17:14:00 +0200

    Call args utils in AnalyticsCounter constructor to process filename and sources list

--- Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,13 +1,23 @@
 package com.hemebiotech.analytics;
 
+import com.hemebiotech.analytics.utils.SymptomUtils;
+import io.vavr.collection.List;
+
 public class AnalyticsCounter {
+
+    SymptomUtils symptomUtils =  new SymptomUtils();
+
+    public static String filename;
+
+    public static List<String> sources;
 
     /**
      * Class constructor
      * @param args are the args received from main
      */
     public AnalyticsCounter(String[] args){
-
+        filename = symptomUtils.getResultFileNameFromArgs(args);
+        sources = symptomUtils.getSourcesFromArgs(args);
     }
 
     /**
