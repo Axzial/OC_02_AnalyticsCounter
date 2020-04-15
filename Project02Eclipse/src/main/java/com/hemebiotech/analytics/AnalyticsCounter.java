2a72fec3a3da10245b57944e060c253991112eee
Victor Brown <victorbrown37@protonmail.com>
2020-04-15 17:38:05 +0200

    Defined launcher

--- Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,10 +1,21 @@
 package com.hemebiotech.analytics;
 
+import com.hemebiotech.analytics.datamanager.SymptomCounter;
+import com.hemebiotech.analytics.filemanager.SymptomFileManager;
+import com.hemebiotech.analytics.objects.RawSymptomFileData;
+import com.hemebiotech.analytics.objects.SymptomFileData;
+import com.hemebiotech.analytics.resultmaker.SymptomResultMaker;
+import com.hemebiotech.analytics.sourcereaders.SymptomFileReader;
 import com.hemebiotech.analytics.utils.SymptomUtils;
 import io.vavr.collection.List;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class AnalyticsCounter {
 
+    SymptomFileManager symptomFileManager =  new SymptomFileManager();
+    SymptomCounter symptomCounter = new SymptomCounter();
     SymptomUtils symptomUtils =  new SymptomUtils();
 
     public static String filename;
@@ -24,6 +35,9 @@ public class AnalyticsCounter {
      * App launcher, executing all needed tasks
      */
     public void launcher(){
-
+        java.util.List<File> files = symptomFileManager.getFilesFromArgs(sources);
+        ArrayList<RawSymptomFileData> rawFileData = new SymptomFileReader(files).getSymptoms();
+        ArrayList<SymptomFileData> dataList = symptomCounter.convertCount(rawFileData);
+        new SymptomResultMaker(dataList);
     }
 }
